78e952ea3b5a220a98c7c48a775941eec552d79d
Sachith Withana <swsachith@gmail.com>
2016-03-23 13:30:18 +0530

    Adding alert suppression for 500 events for unusual IP alert

--- product/integration/tests-integration/src/test/java/org/wso2/analytics/apim/integration/tests/apim/analytics/UnusualIPAccessTestCase.java
+++ product/integration/tests-integration/src/test/java/org/wso2/analytics/apim/integration/tests/apim/analytics/UnusualIPAccessTestCase.java
@@ -35,14 +35,18 @@ public class UnusualIPAccessTestCase extends APIMAnalyticsBaseTestCase {
     private final String TEST_RESOURCE_PATH = "unusualIPAccess";
     private final String PUBLISHER_FILE = "logger.xml";
     private final String ALERT_TABLE_NAME = "IPACCESSSUMMARY";
+    private final int MAX_TRIES = 5;
 
     @BeforeClass(alwaysRun = true)
     public void setup() throws Exception {
         super.init();
         // deploy the publisher xml file
         deployPublisher(TEST_RESOURCE_PATH, PUBLISHER_FILE);
+        if (isTableExist(-1234, STREAM_NAME.replace('.', '_'))) {
+            deleteData(-1234, STREAM_NAME.replace('.', '_'));
+        }
         // publish the csv data
-        pubishEventsFromCSV(TEST_RESOURCE_PATH, "sim.csv", getStreamId(STREAM_NAME, STREAM_VERSION), 100);
+        pubishEventsFromCSV(TEST_RESOURCE_PATH, "sim.csv", getStreamId(STREAM_NAME, STREAM_VERSION), 200);
     }
 
     @AfterClass(alwaysRun = true)
@@ -51,7 +55,45 @@ public class UnusualIPAccessTestCase extends APIMAnalyticsBaseTestCase {
         undeployPublisher(PUBLISHER_FILE);
     }
 
-    @Test(groups = "wso2.analytics.apim", description = "Test New IP detected Alert")
+    @Test(groups = "wso2.analytics.apim", description = "Test if the Simulation data has been published")
+    public void testSimulationDataSent() throws Exception {
+        int i = 0;
+        long requestEventCount = 0;
+        boolean eventsPublished = false;
+        while (i < MAX_TRIES) {
+            Thread.sleep(2000);
+            requestEventCount = getRecordCount(-1234, STREAM_NAME.replace('.', '_'));
+            eventsPublished = (requestEventCount == 500);
+            if (eventsPublished) {
+                break;
+            }
+            i++;
+        }
+        Assert.assertTrue(eventsPublished, "Simulation events did not get published!");
+    }
+
+    @Test(groups = "wso2.analytics.apim", description = "Tests if it waits for the provided request count to" +
+            " proceed with alerting", dependsOnMethods = "testSimulationDataSent")
+    public void testAlertSuppressionCount() throws Exception {
+
+        int beforeCount = logViewerClient.getAllRemoteSystemLogs().length;
+
+        EventDto eventDto = new EventDto();
+        eventDto.setEventStreamId(getStreamId(STREAM_NAME, STREAM_VERSION));
+        eventDto.setAttributeValues(new String[]{"external", "tC3RKfeSoUetfMy4_o6KLAk7fX4a", "/calc/1.0", "CalculatorAPI:v1.0"
+                , "CalculatorAPI", "/search", "/search", "GET", "1", "1", "1455785133344", "sachith@carbon.super", "carbon.super",
+                "10.100.7.100", "apim@carbon.super", "DefaultApplication", "1", "chrome", "Unlimited", "False", "192.168.7.1"});
+        publishEvent(eventDto);
+        Thread.sleep(5000);
+
+        boolean newIpDetectedAlertFound = isAlertReceived(beforeCount, "\"type\":\"UnusualIPAccessAlert\"," +
+                "\"msg\":\"A request from a new IP detected! IP: 192.168.7.1\",\"ip\":\"192.168.7.1\"," +
+                "\"consumerKey\":\"tC3RKfeSoUetfMy4_o6KLAk7fX4a\",\"userId\":\"sachith@carbon.super\"," +
+                "\"requestTime\":1455785133344,");
+        Assert.assertFalse(newIpDetectedAlertFound, "Request count alert suppression does not work");
+    }
+
+    @Test(groups = "wso2.analytics.apim", description = "Test New IP detected Alert", dependsOnMethods = "testAlertSuppressionCount")
     public void testNewIPDetectedAlert() throws Exception {
 
         int beforeCount = logViewerClient.getAllRemoteSystemLogs().length;
