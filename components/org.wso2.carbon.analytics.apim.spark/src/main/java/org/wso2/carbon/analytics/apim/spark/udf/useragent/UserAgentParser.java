24eb304d6d7b8eef2e9dc2c3f8579cb4248fdc2b
Janaka Ranabahu <janaka@wso2.com>
2016-03-29 11:57:57 +0530

    Adding the initial version of userAgentParser

---
+++ components/org.wso2.carbon.analytics.apim.spark/src/main/java/org/wso2/carbon/analytics/apim/spark/udf/useragent/UserAgentParser.java
@@ -0,0 +1,83 @@
+/*
+*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+package org.wso2.carbon.analytics.apim.spark.udf.useragent;
+
+import org.wso2.carbon.analytics.spark.core.udf.CarbonUDF;
+import ua_parser.OS;
+import ua_parser.Parser;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import ua_parser.UserAgent;
+
+import java.io.IOException;
+
+public class UserAgentParser implements CarbonUDF {
+    private static final Log log =  LogFactory.getLog(UserAgentParser.class);
+    private static volatile Parser uaParser;
+
+    private Parser getInstance(){
+        if(uaParser == null){
+            synchronized (this){
+                if(uaParser == null){
+                    try {
+                        uaParser = new Parser();
+                    } catch (IOException e) {
+                        log.error("Unable to initialize the user agent parser", e);
+                    }
+                }
+            }
+        }
+        return uaParser;
+    }
+    /**
+     * This method would extract the Operating system form the given User-Agent String.
+     *
+     * @param userAgent The User-Agent string that is been sent.
+     *                  Ex:- Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/48.0.2564.116 Chrome/48.0.2564.116 Safari/537.36
+     * @return The name of the Operating system extracted from the User-Agent.
+     */
+    public String getOSFromUserAgent(String userAgent){
+        Parser parser = getInstance();
+        if (parser != null) {
+            OS operatingSystem = parser.parseOS(userAgent);
+            if (operatingSystem != null) {
+                return operatingSystem.family;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * This method would extract the browser form the given User-Agent String.
+     *
+     * @param userAgent The User-Agent string that is been sent.
+     *                  Ex:- Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/48.0.2564.116 Chrome/48.0.2564.116 Safari/537.36
+     * @return The name of the browser extracted from the User-Agent.
+     */
+    public String getBrowserFromUserAgent(String userAgent){
+        Parser parser = getInstance();
+        if (parser != null) {
+            UserAgent agent = parser.parseUserAgent(userAgent);
+            if (agent != null) {
+                return agent.family;
+            }
+        }
+        return null;
+    }
+}
