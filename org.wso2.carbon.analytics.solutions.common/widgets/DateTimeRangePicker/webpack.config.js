d1018b4d3ec4b1211cd2697d992feb01a687228d
Niveathika <niveathika@wso2.com>
2018-08-23 17:55:45 +0530

    Externalise react in DateTimeRangePicker

--- org.wso2.carbon.analytics.solutions.common/widgets/DateTimeRangePicker/webpack.config.js
+++ org.wso2.carbon.analytics.solutions.common/widgets/DateTimeRangePicker/webpack.config.js
@@ -17,23 +17,22 @@
  */
 
 const path = require('path');
-const webpack = require('webpack');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, './src'),
     entry: {
-        index: './DateRangePicker.jsx'
+        index: './DateRangePicker.jsx',
     },
     output: {
         path: path.resolve(__dirname, './dist/DateRangePicker/'),
-        filename: 'DateRangePicker.js'
+        filename: 'DateRangePicker.js',
     },
     module: {
         loaders: [
             {
                 test: /\.html$/,
-                use: [{loader: 'html-loader'}]
+                use: [{ loader: 'html-loader' }],
             },
             {
                 test: /\.js$/,
@@ -42,44 +41,45 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         query: {
-                            presets: ['es2015', 'react']
-                        }
-                    }
-                ]
+                            presets: ['es2015', 'react'],
+                        },
+                    },
+                ],
             },
             {
                 test: /\.(png|jpg|svg|cur|gif|eot|svg|ttf|woff|woff2)$/,
-                use: ['url-loader']
+                use: ['url-loader'],
             },
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015', 'react']
-                }
+                    presets: ['es2015', 'react'],
+                },
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.scss$/,
-                use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'sass-loader'}]
-            }
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+            },
 
-        ]
+        ],
     },
     plugins: [
         new CopyWebpackPlugin([
-            {from: path.resolve(__dirname, './src/resources/')}
-        ])
+            { from: path.resolve(__dirname, './src/resources/') },
+        ]),
     ],
     resolve: {
-        extensions: ['.js', '.json', '.jsx', '.scss']
+        extensions: ['.js', '.json', '.jsx', '.scss'],
     },
     devServer: {
         contentBase: path.join(__dirname, 'public'),
-        publicPath: '/dist/'
-    }
+        publicPath: '/dist/',
+    },
+    externals: { react: 'React' },
 };
