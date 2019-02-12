5e0f7865d0858514605f10418c75d20ee82545a9
Sandalu Kalpanee <sandalukalpanee@gmail.com>
2019-02-12 08:55:17 +0530

    Add APIMSubscriptions widget

---
+++ org.wso2.analytics.solutions.apim.analytics/widgets/APIMSubscriptions/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        index: './Subscriptions.jsx',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist/Subscriptions'),
+        filename: 'Subscriptions.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                use: [{ loader: 'html-loader' }],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ['es2015', 'react'],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg|svg|cur|gif|eot|ttf|woff|woff2)$/,
+                use: ['url-loader'],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-2'],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+            },
+
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: path.resolve(__dirname, './src/resources/') },
+        ]),
+    ],
+    resolve: {
+        extensions: ['.js', '.json', '.jsx', '.scss'],
+    },
+    externals: { react: 'React', 'react-dom': 'ReactDOM' },
+};
