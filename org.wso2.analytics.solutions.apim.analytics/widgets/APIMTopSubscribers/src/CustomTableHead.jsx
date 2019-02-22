b30b0685f097b86d75db244718b76e5e64675446
Sandalu Kalpanee <sandalukalpanee@gmail.com>
2019-02-22 19:17:41 +0530

    Add APIMTopSubscribers widget

---
+++ org.wso2.analytics.solutions.apim.analytics/widgets/APIMTopSubscribers/src/CustomTableHead.jsx
@@ -0,0 +1,94 @@
+/*
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import TableHead from '@material-ui/core/TableHead';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Tooltip from '@material-ui/core/Tooltip';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+
+const rows = [
+    {
+        id: 'creator', numeric: false, disablePadding: false, label: 'table.heading.creator',
+    },
+    {
+        id: 'subcount', numeric: true, disablePadding: false, label: 'table.heading.subcount',
+    },
+];
+
+/**
+ * Create React Component for Custom Table Head
+ */
+export default class CustomTableHead extends React.Component {
+    createSortHandler = property => (event) => {
+        const { onRequestSort } = this.props;
+        onRequestSort(event, property);
+    };
+
+    /**
+     * Render the Custom Table Head
+     * @return {ReactElement} customTableHead
+     */
+    render() {
+        const { order, orderBy } = this.props;
+
+        return (
+            <TableHead>
+                <TableRow>
+                    {rows.map((row) => {
+                        return (
+                            <TableCell
+                                key={row.id}
+                                numeric={row.numeric}
+                                padding={row.disablePadding ? 'none' : 'default'}
+                                sortDirection={orderBy === row.id ? order : false}
+                            >
+                                <Tooltip
+                                    title={<FormattedMessage id='sort.label.title' defaultMessage='Sort' />}
+                                    placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                                    enterDelay={300}
+                                >
+                                    <TableSortLabel
+                                        active={orderBy === row.id}
+                                        direction={order}
+                                        onClick={this.createSortHandler(row.id)}
+                                    >
+                                        <FormattedMessage
+                                            id={row.label}
+                                            defaultMessage={row.label.split('table.heading.')[1].toUpperCase()}
+                                        />
+                                    </TableSortLabel>
+                                </Tooltip>
+                            </TableCell>
+                        );
+                    }, this)}
+                </TableRow>
+            </TableHead>
+        );
+    }
+}
+
+CustomTableHead.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.string.isRequired,
+    orderBy: PropTypes.string.isRequired,
+};
