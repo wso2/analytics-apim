ea6d3a3f3c0a1d1fdf2f6ad852e4e42eac688bef
Sandalu Kalpanee <sandalukalpanee@gmail.com>
2019-02-23 09:09:10 +0530

    Add APIMOverallApiStats widget

---
+++ org.wso2.analytics.solutions.apim.analytics/widgets/APIMOverallApiStats/src/APIMOverallApiStatsWidget.jsx
@@ -0,0 +1,342 @@
+/*
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React from 'react';
+import Widget from '@wso2-dashboards/widget';
+import { Scrollbars } from 'react-custom-scrollbars';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import cloneDeep from 'lodash/cloneDeep';
+import Axios from 'axios';
+import {
+    defineMessages, IntlProvider, FormattedMessage,
+} from 'react-intl';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+import ApiAvailability from './ApiAvailability';
+import CustomTable from './CustomTable';
+
+const darkTheme = createMuiTheme({
+    palette: {
+        type: 'dark',
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+const lightTheme = createMuiTheme({
+    palette: {
+        type: 'light',
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+/**
+ * Language
+ * @type {string}
+ */
+const language = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage;
+
+/**
+ * Language without region code
+ */
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+/**
+ * Create React Component for APIM Overall Api Stats
+ * @class APIMOverallApiStatsWidget
+ * @extends {Widget}
+ */
+class APIMOverallApiStatsWidget extends Widget {
+    /**
+     * Creates an instance of APIMOverallApiStatsWidget.
+     * @param {any} props @inheritDoc
+     * @memberof APIMOverallApiStatsWidget
+     */
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            width: this.props.width,
+            height: this.props.height,
+            availableApiData: [],
+            legendData: [],
+            topApiIdData: [],
+            topApiNameData: [],
+            localeMessages: null,
+        };
+
+        this.styles = {
+            loadingIcon: {
+                margin: 'auto',
+                display: 'block',
+            },
+            paper: {
+                padding: '5%',
+                border: '2px solid #4555BB',
+            },
+            paperWrapper: {
+                margin: 'auto',
+                width: '50%',
+                marginTop: '20%',
+            },
+            headingWrapper: {
+                height: '10%',
+                margin: 'auto',
+                width: '90%',
+            },
+        };
+
+        this.assembleApiAvailableQuery = this.assembleApiAvailableQuery.bind(this);
+        this.assembleAPIDataQuery = this.assembleAPIDataQuery.bind(this);
+        this.assembleTopAPIQuery = this.assembleTopAPIQuery.bind(this);
+        this.handleApiAvailableReceived = this.handleApiAvailableReceived.bind(this);
+        this.handleAPIDataReceived = this.handleAPIDataReceived.bind(this);
+        this.handleTopAPIReceived = this.handleTopAPIReceived.bind(this);
+        this.loadLocale = this.loadLocale.bind(this);
+    }
+
+    componentDidMount() {
+        const { widgetID } = this.props;
+        const locale = languageWithoutRegionCode || language;
+        this.loadLocale(locale);
+
+        super.getWidgetConfiguration(widgetID)
+            .then((message) => {
+                this.setState({
+                    providerConfig: message.data.configs.providerConfig,
+                }, this.assembleApiAvailableQuery);
+            })
+            .catch((error) => {
+                console.error("Error occurred when loading widget '" + widgetID + "'. " + error);
+                this.setState({
+                    faultyProviderConfig: true,
+                });
+            });
+    }
+
+    componentWillUnmount() {
+        super.getWidgetChannelManager().unsubscribeWidget(this.props.id);
+    }
+
+    /**
+     * Load locale file.
+     * @param {string} locale Locale name
+     * @memberof APIMOverallApiStatsWidget
+     */
+    loadLocale(locale) {
+        Axios.get(`${window.contextPath}/public/extensions/widgets/APIMOverallApiStats/locales/${locale}.json`)
+            .then((response) => {
+                this.setState({ localeMessages: defineMessages(response.data) });
+            })
+            .catch(error => console.error(error));
+    }
+
+    /**
+     * Formats the siddhi query - apiavailablequery
+     * @memberof APIMOverallApiStatsWidget
+     * */
+    assembleApiAvailableQuery() {
+        const { providerConfig } = this.state;
+
+        const dataProviderConfigs = cloneDeep(providerConfig);
+        dataProviderConfigs.configs.config.queryData.query = dataProviderConfigs.configs.config.queryData.apiavailablequery;
+        super.getWidgetChannelManager().subscribeWidget(this.props.id, this.handleApiAvailableReceived, dataProviderConfigs);
+    }
+
+    /**
+     * Formats data received from assembleApiAvailableQuery
+     * @param {object} message - data retrieved
+     * @memberof APIMOverallApiStatsWidget
+     * */
+    handleApiAvailableReceived(message) {
+        const { data } = message;
+
+        if (data) {
+            const legendData = [];
+            data.forEach((dataUnit) => {
+                if (!legendData.includes({ name: dataUnit[0] })) {
+                    legendData.push({ name: dataUnit[0] });
+                }
+            });
+            this.setState({ legendData, availableApiData: data });
+        }
+        super.getWidgetChannelManager().unsubscribeWidget(this.props.id);
+        this.assembleTopAPIQuery();
+    }
+
+    /**
+     * Formats the siddhi query - topapiquery
+     * @memberof APIMOverallApiStatsWidget
+     * */
+    assembleTopAPIQuery() {
+        const { providerConfig } = this.state;
+
+        const dataProviderConfigs = cloneDeep(providerConfig);
+        dataProviderConfigs.configs.config.queryData.query = dataProviderConfigs.configs.config.queryData.topapiquery;
+        super.getWidgetChannelManager().subscribeWidget(this.props.id, this.handleTopAPIReceived, dataProviderConfigs);
+    }
+
+    /**
+     * Formats data received from assembleTopAPIQuery
+     * @param {object} message - data retrieved
+     * @memberof APIMOverallApiStatsWidget
+     * */
+    handleTopAPIReceived(message) {
+        const { data } = message;
+
+        if (data) {
+            this.setState({ topApiIdData: data });
+        }
+        super.getWidgetChannelManager().unsubscribeWidget(this.props.id);
+        this.assembleAPIDataQuery();
+    }
+
+    /**
+     * Formats the siddhi query - apilistquery
+     * @memberof APIMOverallApiStatsWidget
+     * */
+    assembleAPIDataQuery() {
+        const { providerConfig } = this.state;
+
+        const dataProviderConfigs = cloneDeep(providerConfig);
+        dataProviderConfigs.configs.config.queryData.query = dataProviderConfigs.configs.config.queryData.apilistquery;
+        super.getWidgetChannelManager().subscribeWidget(this.props.id, this.handleAPIDataReceived, dataProviderConfigs);
+    }
+
+    /**
+     * Formats data received from assembleAPIDataQuery
+     * @param {object} message - data retrieved
+     * @memberof APIMOverallApiStatsWidget
+     * */
+    handleAPIDataReceived(message) {
+        const { data } = message;
+        const { topApiIdData } = this.state;
+        if (data) {
+            let counter = 0;
+            const topApiNameData = [];
+            topApiIdData.forEach((apiIdData) => {
+                counter += 1;
+                const apiID = apiIdData[0];
+                let apiName = '';
+                data.forEach((dataUnit) => {
+                    if (dataUnit[0] === apiID) {
+                        apiName = dataUnit[1] + ' ' + dataUnit[2];
+                    }
+                });
+                topApiNameData.push({ id: counter, apiname: apiName, ratings: apiIdData[1] });
+            });
+            this.setState({ topApiNameData });
+        }
+        super.getWidgetChannelManager().unsubscribeWidget(this.props.id);
+    }
+
+    /**
+     * @inheritDoc
+     * @returns {ReactElement} Render the APIM Overall Api Stats widget
+     * @memberof APIMOverallApiStatsWidget
+     */
+    render() {
+        const {
+            localeMessages, faultyProviderConfig, height, availableApiData, legendData, topApiNameData,
+        } = this.state;
+        const {
+            loadingIcon, paper, paperWrapper, headingWrapper,
+        } = this.styles;
+        const themeName = this.props.muiTheme.name;
+        const availabilityProps = { availableApiData, legendData };
+
+        if (!localeMessages) {
+            return (<CircularProgress style={loadingIcon} />);
+        }
+        return (
+            <IntlProvider locale={languageWithoutRegionCode} messages={localeMessages}>
+                {
+                    faultyProviderConfig ? (
+                        <div
+                            style={paperWrapper}
+                        >
+                            <Paper
+                                elevation={1}
+                                style={paper}
+                            >
+                                <Typography variant='h5' component='h3'>
+                                    <FormattedMessage id='config.error.heading' defaultMessage='Configuration Error !' />
+                                </Typography>
+                                <Typography component='p'>
+                                    <FormattedMessage
+                                        id='config.error.body'
+                                        defaultMessage='Cannot fetch provider configuration for APIM Overall Api Stats widget'
+                                    />
+                                </Typography>
+                            </Paper>
+                        </div>
+                    ) : (
+                        <MuiThemeProvider
+                            theme={themeName === 'dark' ? darkTheme : lightTheme}
+                        >
+                            <Scrollbars
+                                style={{ height }}
+                            >
+                                <div style={{
+                                    backgroundColor: themeName === 'dark' ? '#0e1e33' : '#fff',
+                                    width: '80%',
+                                    margin: '5% auto',
+                                    padding: '10% 5%',
+                                }}
+                                >
+                                    <div style={headingWrapper}>
+                                        <h3 style={{
+                                            borderBottom: themeName === 'dark' ? '1px solid #fff' : '1px solid #02212f',
+                                            paddingBottom: '10px',
+                                            margin: 'auto',
+                                            textAlign: 'left',
+                                            fontWeight: 'normal',
+                                            letterSpacing: 1.5,
+                                        }}
+                                        >
+                                            <FormattedMessage id='widget.heading' defaultMessage='OVERALL API STATS' />
+                                        </h3>
+                                    </div>
+                                    <div>
+                                        <div style={{
+                                            marginTop: '10%',
+                                            marginBottom: '10%',
+                                            background: themeName === 'dark' ? '#162638' : '#f7f7f7',
+                                            padding: '5%',
+                                        }}
+                                        >
+                                            <ApiAvailability {...availabilityProps} />
+                                        </div>
+                                        <CustomTable data={topApiNameData} />
+                                    </div>
+                                </div>
+                            </Scrollbars>
+                        </MuiThemeProvider>
+                    )
+                }
+            </IntlProvider>
+        );
+    }
+}
+
+global.dashboard.registerWidget('APIMOverallApiStats', APIMOverallApiStatsWidget);
