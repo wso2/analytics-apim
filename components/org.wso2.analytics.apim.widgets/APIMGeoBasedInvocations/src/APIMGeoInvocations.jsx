b92453022f0370810ecc51110f0150c2350d96cd
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2019-10-22 22:56:33 +0530

    Fix geo location widget not showing data

--- components/org.wso2.analytics.apim.widgets/APIMGeoBasedInvocations/src/APIMGeoInvocations.jsx
+++ components/org.wso2.analytics.apim.widgets/APIMGeoBasedInvocations/src/APIMGeoInvocations.jsx
@@ -20,144 +20,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Scrollbars } from 'react-custom-scrollbars';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
-import APIMGeoInvocationsData from './APIMGeoInvocationsData';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import VizG from 'react-vizgrammar';
 
 /**
- * React Component for APIM Geo Based Invocations widget body
+ * React Component for APIM Geo Based Invocations widget data
  * @param {any} props @inheritDoc
- * @returns {ReactElement} Render the APIM Geo Based Invocations widget body
+ * @returns {ReactElement} Render the APIM Geo Based Invocations widget data
  */
 export default function APIMGeoInvocations(props) {
     const {
-        themeName, chartConfig, metadata, height, width, apiCreatedBy, apiSelected, apiVersion,
-        geoData, apilist, versionlist, apiCreatedHandleChange, apiSelectedHandleChange, apiVersionHandleChange,
+        themeName, chartConfig, metadata, width, geoData, inProgress, height
     } = props;
     const styles = {
-        headingWrapper: {
-            height: '10%',
-            margin: 'auto',
-            width: '100%',
+        paperWrapper: {
+            height: '75%',
         },
-        formWrapper: {
-            width: '100%',
-            height: '10%',
+        paper: {
+            background: '#969696',
+            width: '75%',
+            padding: '4%',
+            border: '1.5px solid #fff',
             margin: 'auto',
+            marginTop: '5%',
         },
-        form: {
+        dataWrapper: {
+            height: '80%',
+        },
+        loadingIcon: {
+            margin: 'auto',
+            display: 'block',
+        },
+        loading: {
             display: 'flex',
-            flexWrap: 'wrap',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height,
         },
-        formControl: {
-            margin: '2%',
-            marginLeft: 0,
-            minWidth: 120,
-        },
-        selectEmpty: {
-            marginTop: 10,
-        },
-    };
-    const geoDataProps = {
-        themeName, chartConfig, metadata, width, geoData,
     };
 
     return (
-        <Scrollbars
-            style={{ height }}
-        >
-            <div
-                style={{
-                    padding: '5%',
-                }}
-            >
-                <div style={styles.headingWrapper}>
-                    <div style={{
-                        borderBottom: themeName === 'dark' ? '1px solid #fff' : '1px solid #02212f',
-                        paddingBottom: '10px',
-                        margin: 'auto',
-                        marginTop: 0,
-                        textAlign: 'left',
-                        fontWeight: 'normal',
-                        letterSpacing: 1.5,
-                    }}
-                    >
-                        <FormattedMessage id='widget.heading' defaultMessage='GEO LOCATION BASED INVOCATIONS' />
-                    </div>
+        <div>
+            { inProgress ? (
+                <div style={styles.loading}>
+                    <CircularProgress style={styles.loadingIcon} />
                 </div>
-                <div style={styles.formWrapper}>
-                    <form style={styles.form}>
-                        <FormControl style={styles.formControl}>
-                            <InputLabel shrink htmlFor='api-createdBy-label-placeholder'>
-                                <FormattedMessage id='createdBy.label' defaultMessage='API Created By' />
-                            </InputLabel>
-                            <Select
-                                value={apiCreatedBy}
-                                onChange={apiCreatedHandleChange}
-                                input={<Input name='apiCreatedBy' id='api-createdBy-label-placeholder' />}
-                                displayEmpty
-                                name='apiCreatedBy'
-                                style={styles.selectEmpty}
+            ) : (
+                <div>
+                    { !geoData || geoData.length === 0 ? (
+                        <div style={styles.paperWrapper}>
+                            <Paper
+                                elevation={1}
+                                style={styles.paper}
                             >
-                                <MenuItem value='All'>
-                                    <FormattedMessage id='all.menuItem' defaultMessage='All' />
-                                </MenuItem>
-                                <MenuItem value='Me'>
-                                    <FormattedMessage id='me.menuItem' defaultMessage='Me' />
-                                </MenuItem>
-                            </Select>
-                        </FormControl>
-                        <FormControl style={styles.formControl}>
-                            <InputLabel shrink htmlFor='apiSelected-label-placeholder'>
-                                <FormattedMessage id='apiName.label' defaultMessage='API Name' />
-                            </InputLabel>
-                            <Select
-                                value={apiSelected}
-                                onChange={apiSelectedHandleChange}
-                                input={<Input name='apiSelected' id='apiSelected-label-placeholder' />}
-                                displayEmpty
-                                name='apiSelected'
-                                style={styles.selectEmpty}
-                            >
-                                {
-                                    apilist.map(option => (
-                                        <MenuItem key={option} value={option}>
-                                            {option}
-                                        </MenuItem>
-                                    ))
-                                }
-                            </Select>
-                        </FormControl>
-                        <FormControl style={styles.formControl}>
-                            <InputLabel shrink htmlFor='apiVersion-label-placeholder'>
-                                <FormattedMessage id='apiVersion.label' defaultMessage='API Version' />
-                            </InputLabel>
-                            <Select
-                                value={apiVersion}
-                                onChange={apiVersionHandleChange}
-                                input={<Input name='apiVersion' id='apiVersion-label-placeholder' />}
-                                displayEmpty
-                                name='apiVersion'
-                                style={styles.selectEmpty}
-                            >
-                                {
-                                    versionlist.map(option => (
-                                        <MenuItem key={option} value={option}>
-                                            {option}
-                                        </MenuItem>
-                                    ))
-                                }
-                            </Select>
-                        </FormControl>
-                    </form>
+                                <Typography variant='h5' component='h3'>
+                                    <FormattedMessage id='nodata.error.heading' defaultMessage='No Data Available !' />
+                                </Typography>
+                                <Typography component='p'>
+                                    <FormattedMessage
+                                        id='nodata.error.body'
+                                        defaultMessage='No data available for the selected options.'
+                                    />
+                                </Typography>
+                            </Paper>
+                        </div>
+                    ) : (
+                        <div style={styles.dataWrapper}>
+                            <VizG
+                                config={chartConfig}
+                                metadata={metadata}
+                                data={geoData}
+                                width={width}
+                                theme={themeName}
+                            />
+                        </div>
+                    )}
                 </div>
-                <APIMGeoInvocationsData {...geoDataProps} />
-            </div>
-        </Scrollbars>
+            )}
+        </div>
     );
 }
 
@@ -165,15 +107,6 @@ APIMGeoInvocations.propTypes = {
     themeName: PropTypes.string.isRequired,
     chartConfig: PropTypes.instanceOf(Object).isRequired,
     metadata: PropTypes.instanceOf(Object).isRequired,
-    height: PropTypes.string.isRequired,
     width: PropTypes.string.isRequired,
-    apiCreatedBy: PropTypes.string.isRequired,
-    apiSelected: PropTypes.string.isRequired,
-    apiVersion: PropTypes.string.isRequired,
     geoData: PropTypes.instanceOf(Object).isRequired,
-    apilist: PropTypes.instanceOf(Object).isRequired,
-    versionlist: PropTypes.instanceOf(Object).isRequired,
-    apiCreatedHandleChange: PropTypes.func.isRequired,
-    apiSelectedHandleChange: PropTypes.func.isRequired,
-    apiVersionHandleChange: PropTypes.func.isRequired,
 };
