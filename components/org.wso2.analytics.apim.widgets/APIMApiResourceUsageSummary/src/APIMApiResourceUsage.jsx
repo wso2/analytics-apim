a4d0cfa77c46d8672123d9af2521a1f4ee736346
ruwiniwj <ruwi1025@gmail.com>
2019-12-10 11:19:46 +0530

    Fix background color in publisher dashboard page 4

--- components/org.wso2.analytics.apim.widgets/APIMApiResourceUsageSummary/src/APIMApiResourceUsage.jsx
+++ components/org.wso2.analytics.apim.widgets/APIMApiResourceUsageSummary/src/APIMApiResourceUsage.jsx
@@ -43,40 +43,31 @@ export default function APIMApiResourceUsage(props) {
     } = props;
     const styles = {
         headingWrapper: {
-            height: '10%',
             margin: 'auto',
-            width: '90%',
+            width: '95%',
         },
         paperWrapper: {
             height: '75%',
         },
         paper: {
-            background: '#969696',
+            background: themeName === 'dark' ? '#969696' : '#E8E8E8',
+            borderColor: themeName === 'dark' ? '#fff' : '#D8D8D8',
             width: '75%',
             padding: '4%',
-            border: '1.5px solid #fff',
-            margin: 'auto',
-            marginTop: '5%',
+            border: '1.5px solid',
+            marginLeft:'5%',
         },
         formWrapper: {
-            width: '90%',
-            height: '10%',
-            margin: 'auto',
+            marginBottom: '5%',
         },
         form: {
             display: 'flex',
             flexWrap: 'wrap',
         },
         formControl: {
-            margin: '5%',
+            marginLeft: '5%',
+            marginTop: '5%',
             minWidth: 120,
-        },
-        textField: {
-            margin: '5%',
-            minWidth: 120,
-        },
-        selectEmpty: {
-            marginTop: 10,
         },
         loadingIcon: {
             margin: 'auto',
@@ -95,7 +86,10 @@ export default function APIMApiResourceUsage(props) {
             style={{ height }}
         >
             <div style={{
-                padding: '5% 5%',
+                backgroundColor: themeName === 'dark' ? '#0e1e33' : '#fff',
+                height,
+                margin: '10px',
+                padding: '20px',
             }}
             >
                 <div style={styles.headingWrapper}>
@@ -124,7 +118,6 @@ export default function APIMApiResourceUsage(props) {
                                 input={<Input name='apiCreatedBy' id='api-createdBy-label-placeholder' />}
                                 displayEmpty
                                 name='apiCreatedBy'
-                                style={styles.selectEmpty}
                             >
                                 <MenuItem value='All'>
                                     <FormattedMessage id='all.menuItem' defaultMessage='All' />
@@ -140,7 +133,7 @@ export default function APIMApiResourceUsage(props) {
                             value={limit}
                             onChange={handleChange}
                             type='number'
-                            style={styles.textField}
+                            style={styles.formControl}
                             InputLabelProps={{
                                 shrink: true,
                             }}
