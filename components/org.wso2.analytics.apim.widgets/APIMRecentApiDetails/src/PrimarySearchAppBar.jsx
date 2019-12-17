d7e99ba631d5f154bf7556e12d307752d1a1f861
Thidasala <thidasala.demintha@gmail.com>
2019-12-17 16:16:29 +0530

    add recent api details widget

---
+++ components/org.wso2.analytics.apim.widgets/APIMRecentApiDetails/src/PrimarySearchAppBar.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable require-jsdoc */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+};
+
+function SimpleAppBar(props) {
+    const { classes } = props;
+
+    return (
+        <div className={classes.root}>
+            <AppBar position='static' color='#27296b' style={{ backgroundColor: '#27296b' }}>
+                <Toolbar>
+                    <Typography variant='h7' color='inherit'>
+            Recent Api Details
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+SimpleAppBar.propTypes = {
+    // eslint-disable-next-line react/forbid-prop-types
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleAppBar);
