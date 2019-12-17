122b401d316d3a9c0a684bcab2a64c8a860bef77
Thidasala <thidasala.demintha@gmail.com>
2019-12-17 12:05:41 +0530

    add APIM Api Latency widget

---
+++ components/org.wso2.analytics.apim.widgets/APIMApiLatency/src/APIMApiLatency.jsx
@@ -0,0 +1,117 @@
+/*
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+ 
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import CustomTable from './CustomTable';
+
+/**
+ * React Component for Api Latency widget body
+ * @param {any} props @inheritDoc
+ * @returns {ReactElement} Render the Api Latency widget body
+ */
+export default function APIMApiLatency(props) {
+    const {
+        themeName, latancyData,
+    } = props;
+  //  console.log(latancyData);
+    const styles = {
+        headingWrapper: {
+            height: '5%',
+            margin: 'auto',
+            paddingTop: '10px',
+            width: '90%',
+        },
+        paperWrapper: {
+            height: '75%',
+        },
+        paper: {
+            background: '#969696',
+            width: '75%',
+            padding: '4%',
+            border: '1.5px solid #fff',
+            margin: 'auto',
+            marginTop: '5%',
+        },
+        selectEmpty: {
+            marginTop: 10,
+        },
+    };
+    if (latancyData == null || latancyData.length === 0) {
+        return (
+            <div style={styles.paperWrapper}>
+                <Paper
+                    elevation={1}
+                    style={styles.paper}
+                >
+                    <Typography variant='h5' component='h3'>
+                        <FormattedMessage id='nodata.error.heading' defaultMessage='No Data Available !' />
+                    </Typography>
+                    <Typography component='p'>
+                        <FormattedMessage
+                            id='nodata.error.body'
+                            defaultMessage='No data available for the selected options.'
+                        />
+                    </Typography>
+                </Paper>
+            </div>
+        );
+    }
+    return (
+            <div 
+                style={{
+                background: themeName === 'light' ? '#fff' : '#162638',
+                width: 'auto',
+                height: 'auto',
+                margin: '3% 3%',
+            }}
+            >
+                <div style={styles.headingWrapper}>
+                    <h3 style={{
+                        borderBottom: themeName === 'dark' ? '1.5px solid #fff' : '1px solid #02212f',
+                        paddingBottom: '5px',
+                        margin: 'auto',
+                        textAlign: 'center',
+                        fontWeight: 'normal',
+                        letterSpacing: 1.5,
+                    }}
+                    >
+                        <FormattedMessage id='widget.heading' defaultMessage='Api Latency' />
+
+                    </h3>
+                </div>
+                <div style={styles.dataWrapper}>
+                    <CustomTable
+                        data={latancyData}
+                    />
+                </div>
+                
+            </div>
+    );
+}
+
+APIMRecentApiTraffic.propTypes = {
+    themeName: PropTypes.string.isRequired,
+    height: PropTypes.string.isRequired,
+    latancyData: PropTypes.instanceOf(Object).isRequired,
+    handleChange: PropTypes.func.isRequired,
+};
---
+++ components/org.wso2.analytics.apim.widgets/APIMApiLatency/src/APIMApiLatencyWidget.jsx
@@ -0,0 +1,250 @@
+/*
+ *  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React from 'react';
+import {
+    defineMessages, IntlProvider, FormattedMessage,
+} from 'react-intl';
+import Axios from 'axios';
+import cloneDeep from 'lodash/cloneDeep';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Widget from '@wso2-dashboards/widget';
+import APIMApiLatency from './APIMApiLatency';
+
+const darkTheme = createMuiTheme({
+    palette: {
+        type: 'dark',
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+const lightTheme = createMuiTheme({
+    palette: {
+        type: 'light',
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+const queryParamKey = 'apilatency';
+
+
+const language = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage;
+
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+
+class APIMApiLatencyWidget extends Widget {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            width: this.props.width,
+            height: this.props.height,
+            localeMessages: null,
+            latancyData: null,
+           // refreshInterval: 60000, // 1min
+        };
+
+        this.styles = {
+            loadingIcon: {
+                margin: 'auto',
+                display: 'block',
+            },
+            paper: {
+                padding: '5%',
+                border: '10px solid #4555BB',
+            },
+            paperWrapper: {
+                margin: 'auto',
+                width: '50%',
+                marginTop: '20%',
+            },
+            inProgress: {
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: this.props.height,
+            },
+        };
+
+        // This will re-size the widget when the glContainer's width is changed.
+        if (this.props.glContainer !== undefined) {
+            this.props.glContainer.on('resize', () => this.setState({
+                width: this.props.glContainer.width,
+                height: this.props.glContainer.height,
+            }));
+        }
+
+
+        this.assemblelatencyQuery = this.assemblelatencyQuery.bind(this);
+        this.handleTotallatencyReceived = this.handleTotallatencyReceived.bind(this);
+        this.loadLocale = this.loadLocale.bind(this);
+        this.handlePublisherParameters = this.handlePublisherParameters.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        const { widgetID, id } = this.props;
+        const locale = languageWithoutRegionCode || language;
+        this.loadLocale(locale);
+
+        super.getWidgetConfiguration(widgetID)
+            .then((message) => {
+                this.setState({
+                    providerConfig: message.data.configs.providerConfig,
+                }, () => super.subscribe(this.handlePublisherParameters));
+            })
+            .catch((error) => {
+                console.error("Error occurred when loading widget '" + widgetID + "'. " + error);
+                this.setState({
+                    faultyProviderConfig: true,
+                });
+            });
+    }
+
+    componentWillUnmount() {
+        const { id } = this.props;
+        super.getWidgetChannelManager().unsubscribeWidget(id);
+    }
+
+    //Load the locale file
+    loadLocale(locale) {
+        Axios.get(`${window.contextPath}/public/extensions/widgets/APIMApiLatency/locales/${locale}.json`)
+            .then((response) => {
+                this.setState({ localeMessages: defineMessages(response.data) });
+            })
+            .catch(error => console.error(error));
+    }
+
+     //Set the date time range
+     handlePublisherParameters(receivedMsg) {
+         console.log(receivedMsg);
+        this.setState({
+            timeFrom: receivedMsg.from,
+            timeTo: receivedMsg.to,
+            perValue: receivedMsg.granularity,
+        }, this.assemblelatencyQuery);
+    }
+
+    //format the siddhi query
+    assemblelatencyQuery() {
+        const { id, widgetID: widgetName } = this.props;
+        const {timeFrom, timeTo, perValue, providerConfig} = this.state;
+
+        const dataProviderConfigs = cloneDeep(providerConfig);
+        dataProviderConfigs.configs.config.queryData.queryName = 'latencyquery';
+        dataProviderConfigs.configs.config.queryData.queryValues = {
+            '{{from}}': timeFrom,
+            '{{to}}': timeTo,
+            '{{per}}': perValue,
+        };
+        super.getWidgetChannelManager()
+            .subscribeWidget(id, widgetName, this.handleTotallatencyReceived, dataProviderConfigs);
+    }
+
+    //Format the data received from the query
+    handleTotallatencyReceived(message) {
+        const { data } = message;
+        console.log(data);
+        const latancyData = [];
+        
+        if (data) {
+            data.forEach(dataunit => {
+                latancyData.push({
+                    ApiName: dataunit[0], maxLatency: dataunit[1]})
+                
+            });
+        }
+
+        this.setState({latancyData});
+       // console.log(latancyData);
+    }
+
+    handleChange(event) {
+        const { id } = this.props;
+
+        this.setQueryParam(event.target.value);
+        super.getWidgetChannelManager().unsubscribeWidget(id);
+        this.assemblelatencyQuery();
+    }
+
+    //Render the Apim Latency Widget
+    render() {
+        const {
+            localeMessages, faultyProviderConf, latancyData, height
+        } = this.state;
+        const {
+            loadingIcon, paper, paperWrapper, inProgress,
+        } = this.styles;
+        const { muiTheme } = this.props;
+        const themeName = muiTheme.name;
+        const apiLatancyProps = { themeName, latancyData, height };
+
+       // console.log(apiLatancyProps);
+
+        if (!localeMessages) {
+            return (
+                <div style={inProgress}>
+                    <CircularProgress style={loadingIcon} />
+                </div>
+            );
+        }
+        return (
+            <IntlProvider locale={languageWithoutRegionCode} messages={localeMessages}>
+                <MuiThemeProvider theme={themeName === 'dark' ? darkTheme : lightTheme}>
+                    {
+                        faultyProviderConf ? (
+                            <div style={paperWrapper}>
+                                <Paper elevation={1} style={paper}>
+                                    <Typography variant='h4' component='h3'>
+                                        <FormattedMessage
+                                            id='config.error.heading'
+                                            defaultMessage='Configuration Error !'
+                                        />
+                                    </Typography>
+                                    <Typography component='p'>
+                                        <FormattedMessage
+                                            id='config.error.body'
+                                            defaultMessage={'Cannot fetch provider configuration for APIM Api '
+                                            + 'Created widget'}
+                                        />
+                                    </Typography>
+                                </Paper>
+                            </div>
+                        ) : (
+                            <APIMApiLatency {...apiLatancyProps}
+                            handleChange={this.handleChange} 
+                            />
+                        )
+                    }
+                </MuiThemeProvider>
+            </IntlProvider>
+        );
+    }
+}
+
+global.dashboard.registerWidget('APIMApiLatency', APIMApiLatencyWidget);
