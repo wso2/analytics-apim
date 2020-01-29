43b41441496527d9713bb78fd2b9a28f2556c2d0
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2020-01-29 23:21:16 +0530

    Add dashboard authorizer component
    This component was earlier inside the carbon dashboards core and now it
    is moved into the Analytics APIM because it is related to APIM Analytics

---
+++ components/org.wso2.analytics.apim.dashboards/src/main/java/org/wso2/analytics/apim/dashboards/bean/TenantIdInfo.java
@@ -0,0 +1,77 @@
+/*
+ *   Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
+
+package org.wso2.analytics.apim.dashboards.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Objects;
+
+/**
+ * Bean class to hold the content of a Tenant Id.
+ */
+public class TenantIdInfo {
+
+    @SerializedName("username")
+    private String username;
+    @SerializedName("tenantDomain")
+    private String tenantDomain;
+    @SerializedName("tenantId")
+    private Integer tenantId;
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTenantDomain() {
+        return tenantDomain;
+    }
+    public void setTenantDomain(String tenantDomain) {
+        this.tenantDomain = tenantDomain;
+    }
+
+    public Integer getTenantId() {
+        return tenantId;
+    }
+    public void setTenantId(Integer tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TenantIdInfo that = (TenantIdInfo) o;
+        return Objects.equals(username, that.username) &&
+                Objects.equals(tenantDomain, that.tenantDomain) &&
+                Objects.equals(tenantId, that.tenantId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, tenantDomain, tenantId);
+    }
+}
