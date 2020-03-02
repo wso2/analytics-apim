7b86a7d3659278a097b7001fd59089199409d152
pahans <pahan@wso2.com>
2020-03-03 00:11:35 +0530

    Improve no data UI

--- components/org.wso2.analytics.apim.widgets/APIMRegisteredAppUsers/src/APIMRegisteredAppUsers.jsx
+++ components/org.wso2.analytics.apim.widgets/APIMRegisteredAppUsers/src/APIMRegisteredAppUsers.jsx
@@ -48,15 +48,12 @@ export default function APIMRegisteredAppUsers(props) {
         },
         paperWrapper: {
             height: '75%',
+            width: '95%',
+            margin: 'auto',
         },
         paper: {
-            background: themeName === 'dark' ? '#969696' : '#E8E8E8',
-            borderColor: themeName === 'dark' ? '#fff' : '#D8D8D8',
-            width: '75%',
+            background: themeName === 'dark' ? '#152638' : '#E8E8E8',
             padding: '4%',
-            border: '1.5px solid',
-            marginLeft: '5%',
-            marginTop: '5%',
         },
         contentDiv: {
             backgroundColor: themeName === 'dark' ? '#0e1e33' : '#fff',
