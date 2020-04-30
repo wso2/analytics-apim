788535feb29ba57d7b069509368781b943f0256b
ruwiniwj <ruwi1025@gmail.com>
2020-04-30 17:42:12 +0530

    add api fault analytics widget

---
+++ components/org.wso2.analytics.apim.widgets/APIMApiFaultAnalytics/src/APIMApiFaultAnalytics.jsx
@@ -0,0 +1,157 @@
+/*
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Scrollbars } from 'react-custom-scrollbars';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import VizG from 'react-vizgrammar';
+import CustomTable from './CustomTable';
+
+/**
+ * React Component for APIM Api Fault Analytics widget body
+ * @param {any} props @inheritDoc
+ * @returns {ReactElement} Render the APIM Api Fault Analytics widget body
+ */
+export default function APIMApiFaultAnalytics(props) {
+    const {
+        themeName, chartConfig, metadata, height, width, inProgress, faultData, tableData,
+    } = props;
+    const styles = {
+        headingWrapper: {
+            margin: 'auto',
+            width: '95%',
+        },
+        dataWrapper: {
+            paddingTop: '20px',
+
+        },
+        tableWrapper: {
+            paddingTop: '20px',
+
+        },
+        paperWrapper: {
+            height: '75%',
+            width: '95%',
+            margin: 'auto',
+        },
+        paper: {
+            background: themeName === 'dark' ? '#152638' : '#E8E8E8',
+            padding: '4%',
+        },
+        loadingIcon: {
+            margin: 'auto',
+            display: 'block',
+        },
+        loading: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height,
+        },
+    };
+
+    return (
+        <Scrollbars style={{
+            height,
+            backgroundColor: themeName === 'dark' ? '#0e1e33' : '#fff',
+        }}
+        >
+            <div
+                style={{
+                    backgroundColor: themeName === 'dark' ? '#0e1e33' : '#fff',
+                    margin: '10px',
+                    padding: '20px',
+                }}
+            >
+                <div style={styles.headingWrapper}>
+                    <div style={{
+                        borderBottom: themeName === 'dark' ? '1px solid #fff' : '1px solid #02212f',
+                        paddingBottom: '10px',
+                        margin: 'auto',
+                        marginTop: 0,
+                        textAlign: 'left',
+                        fontWeight: 'normal',
+                        letterSpacing: 1.5,
+                    }}
+                    >
+                        <FormattedMessage id='widget.heading' defaultMessage='API FAULTY INVOCATIONS OVER TIME' />
+                    </div>
+                </div>
+                { inProgress ? (
+                    <div style={styles.loading}>
+                        <CircularProgress style={styles.loadingIcon} />
+                    </div>
+                ) : (
+                    <div>
+                        { !faultData || faultData.length === 0 ? (
+                            <div style={styles.paperWrapper}>
+                                <Paper
+                                    elevation={1}
+                                    style={styles.paper}
+                                >
+                                    <Typography variant='h5' component='h3'>
+                                        <FormattedMessage
+                                            id='nodata.error.heading'
+                                            defaultMessage='No Data Available !'
+                                        />
+                                    </Typography>
+                                    <Typography component='p'>
+                                        <FormattedMessage
+                                            id='nodata.error.body'
+                                            defaultMessage='No data available for the selected options.'
+                                        />
+                                    </Typography>
+                                </Paper>
+                            </div>
+                        ) : (
+                            <div style={styles.dataWrapper}>
+                                <VizG
+                                    config={chartConfig}
+                                    metadata={metadata}
+                                    data={faultData}
+                                    width={width}
+                                />
+                                <div style={styles.tableWrapper}>
+                                    <CustomTable
+                                        data={tableData}
+                                    />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+        </Scrollbars>
+    );
+}
+
+APIMApiFaultAnalytics.propTypes = {
+    themeName: PropTypes.string.isRequired,
+    chartConfig: PropTypes.instanceOf(Object).isRequired,
+    metadata: PropTypes.instanceOf(Object).isRequired,
+    height: PropTypes.string.isRequired,
+    width: PropTypes.string.isRequired,
+    faultData: PropTypes.instanceOf(Object).isRequired,
+    tableData: PropTypes.instanceOf(Object).isRequired,
+    inProgress: PropTypes.bool.isRequired,
+};
