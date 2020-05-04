efe4075e140e20825e43e8c3e8678d21a28740ac
ruwiniwj <ruwi1025@gmail.com>
2020-05-04 15:06:33 +0530

    add api throttle analytics widget

---
+++ components/org.wso2.analytics.apim.widgets/APIMApiThrottleAnalytics/src/APIMApiThrottleAnalytics.jsx
@@ -0,0 +1,163 @@
+/*
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Scrollbars } from 'react-custom-scrollbars';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import VizG from 'react-vizgrammar';
+import CustomTable from './CustomTable';
+
+/**
+ * React Component for APIM Api Throttle Analytics widget body
+ * @param {any} props @inheritDoc
+ * @returns {ReactElement} Render the APIM Api Throttle Analytics widget body
+ */
+export default function APIMApiThrottleAnalytics(props) {
+    const {
+        themeName, chartConfig, metadata, height, width, inProgress, throttleData, tableData,
+    } = props;
+    const styles = {
+        headingWrapper: {
+            margin: 'auto',
+            width: '95%',
+        },
+        dataWrapper: {
+            paddingTop: '20px',
+
+        },
+        chartWrapper: {
+            paddingTop: '20px',
+            marginLeft: '10px',
+            width: '95%',
+        },
+        tableWrapper: {
+            paddingTop: '20px',
+        },
+        paperWrapper: {
+            height: '75%',
+            width: '95%',
+            margin: 'auto',
+        },
+        paper: {
+            background: themeName === 'dark' ? '#152638' : '#E8E8E8',
+            padding: '4%',
+        },
+        loadingIcon: {
+            margin: 'auto',
+            display: 'block',
+        },
+        loading: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height,
+        },
+    };
+
+    return (
+        <Scrollbars style={{
+            height,
+            backgroundColor: themeName === 'dark' ? '#0e1e33' : '#fff',
+        }}
+        >
+            <div
+                style={{
+                    backgroundColor: themeName === 'dark' ? '#0e1e33' : '#fff',
+                    margin: '10px',
+                    padding: '20px',
+                }}
+            >
+                <div style={styles.headingWrapper}>
+                    <div style={{
+                        borderBottom: themeName === 'dark' ? '1px solid #fff' : '1px solid #02212f',
+                        paddingBottom: '10px',
+                        margin: 'auto',
+                        marginTop: 0,
+                        textAlign: 'left',
+                        fontWeight: 'normal',
+                        letterSpacing: 1.5,
+                    }}
+                    >
+                        <FormattedMessage id='widget.heading' defaultMessage='API THROT ANALYTICS' />
+                    </div>
+                </div>
+                { inProgress ? (
+                    <div style={styles.loading}>
+                        <CircularProgress style={styles.loadingIcon} />
+                    </div>
+                ) : (
+                    <div>
+                        { !throttleData || throttleData.length === 0 ? (
+                            <div style={styles.paperWrapper}>
+                                <Paper
+                                    elevation={1}
+                                    style={styles.paper}
+                                >
+                                    <Typography variant='h5' component='h3'>
+                                        <FormattedMessage
+                                            id='nodata.error.heading'
+                                            defaultMessage='No Data Available !'
+                                        />
+                                    </Typography>
+                                    <Typography component='p'>
+                                        <FormattedMessage
+                                            id='nodata.error.body'
+                                            defaultMessage='No data available for the selected options.'
+                                        />
+                                    </Typography>
+                                </Paper>
+                            </div>
+                        ) : (
+                            <div style={styles.dataWrapper}>
+                                <div style={styles.chartWrapper}>
+                                    <VizG
+                                        config={chartConfig}
+                                        metadata={metadata}
+                                        data={throttleData}
+                                        width={width}
+                                    />
+                                </div>
+                                <div style={styles.tableWrapper}>
+                                    <CustomTable
+                                        data={tableData}
+                                    />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+        </Scrollbars>
+    );
+}
+
+APIMApiThrottleAnalytics.propTypes = {
+    themeName: PropTypes.string.isRequired,
+    chartConfig: PropTypes.instanceOf(Object).isRequired,
+    metadata: PropTypes.instanceOf(Object).isRequired,
+    height: PropTypes.string.isRequired,
+    width: PropTypes.string.isRequired,
+    throttleData: PropTypes.instanceOf(Object).isRequired,
+    tableData: PropTypes.instanceOf(Object).isRequired,
+    inProgress: PropTypes.bool.isRequired,
+};
---
+++ components/org.wso2.analytics.apim.widgets/APIMApiThrottleAnalytics/src/APIMApiThrottleAnalyticsWidget.jsx
@@ -0,0 +1,363 @@
+/*
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+import React from 'react';
+import {
+    defineMessages, IntlProvider, FormattedMessage, addLocaleData,
+} from 'react-intl';
+import Axios from 'axios';
+import cloneDeep from 'lodash/cloneDeep';
+import Moment from 'moment';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Widget from '@wso2-dashboards/widget';
+import APIMApiThrottleAnalytics from './APIMApiThrottleAnalytics';
+
+const darkTheme = createMuiTheme({
+    palette: {
+        type: 'dark',
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+const lightTheme = createMuiTheme({
+    palette: {
+        type: 'light',
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+/**
+ * Language
+ * @type {string}
+ */
+const language = (navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage;
+
+/**
+ * Language without region code
+ */
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+/**
+ * Create React Component for APIM Api Throttle Analytics widget
+ * @class APIMApiThrottleAnalyticsWidget
+ * @extends {Widget}
+ */
+class APIMApiThrottleAnalyticsWidget extends Widget {
+    /**
+     * Creates an instance of APIMApiThrottleAnalyticsWidget.
+     * @param {any} props @inheritDoc
+     * @memberof APIMApiThrottleAnalyticsWidget
+     */
+    constructor(props) {
+        super(props);
+
+        this.chartConfig = {
+            x: 'REQUEST_TIME',
+            charts: [
+                {
+                    type: 'line',
+                    y: 'THROTTLE_COUNT',
+                    fill: '#958E94',
+                },
+            ],
+            maxLength: 60,
+            width: 800,
+            height: 400,
+            timeFormat: '%d-%b-%y %H:%M',
+            tipTimeFormat: '%Y-%m-%d %H:%M:%S',
+            style: {
+                xAxisTickAngle: -8,
+                tickLabelColor: '#a7b0c8',
+                axisLabelColor: '#a7b0c8',
+                axisTextSize: 10,
+            },
+        };
+
+        this.metadata = {
+            names: ['THROTTLE_COUNT', 'REQUEST_TIME'],
+            types: ['linear', 'time'],
+        };
+
+        this.styles = {
+            formControl: {
+                margin: 5,
+                minWidth: 120,
+            },
+            selectEmpty: {
+                marginTop: 10,
+            },
+            form: {
+                display: 'flex',
+                flexWrap: 'wrap',
+            },
+            paper: {
+                padding: '5%',
+                border: '2px solid #4555BB',
+            },
+            paperWrapper: {
+                margin: 'auto',
+                width: '50%',
+                marginTop: '20%',
+            },
+        };
+
+        this.state = {
+            width: this.props.width,
+            height: this.props.height,
+            throttleData: null,
+            tableData: null,
+            inProgress: true,
+            metadata: this.metadata,
+            chartConfig: this.chartConfig,
+            dimension: null,
+            selectedOptions: [],
+        };
+
+        // This will re-size the widget when the glContainer's width is changed.
+        if (this.props.glContainer !== undefined) {
+            this.props.glContainer.on('resize', () => this.setState({
+                width: this.props.glContainer.width,
+                height: this.props.glContainer.height,
+            }));
+        }
+
+        this.handleDataReceived = this.handleDataReceived.bind(this);
+        this.handlePublisherParameters = this.handlePublisherParameters.bind(this);
+        this.assembleMainQuery = this.assembleMainQuery.bind(this);
+    }
+
+    componentWillMount() {
+        const locale = (languageWithoutRegionCode || language || 'en');
+        this.loadLocale(locale).catch(() => {
+            this.loadLocale().catch(() => {
+                // TODO: Show error message.
+            });
+        });
+    }
+
+    componentDidMount() {
+        const { widgetID } = this.props;
+
+        super.getWidgetConfiguration(widgetID)
+            .then((message) => {
+                this.setState({
+                    providerConfig: message.data.configs.providerConfig,
+                }, () => super.subscribe(this.handlePublisherParameters));
+            })
+            .catch((error) => {
+                console.error("Error occurred when loading widget '" + widgetID + "'. " + error);
+                this.setState({
+                    faultyProviderConfig: true,
+                });
+            });
+    }
+
+    componentWillUnmount() {
+        const { id } = this.props;
+        super.getWidgetChannelManager().unsubscribeWidget(id);
+    }
+
+    /**
+     * Load locale file.
+     * @param {string} locale Locale name
+     * @memberof APIMApiThrottleAnalyticsWidget
+     */
+    loadLocale(locale = 'en') {
+        return new Promise((resolve, reject) => {
+            Axios
+                .get(`${window.contextPath}/public/extensions/widgets/APIMApiThrottleAnalytics/locales/${locale}.json`)
+                .then((response) => {
+                    // eslint-disable-next-line global-require, import/no-dynamic-require
+                    addLocaleData(require(`react-intl/locale-data/${locale}`));
+                    this.setState({ localeMessages: defineMessages(response.data) });
+                    resolve();
+                })
+                .catch(error => reject(error));
+        });
+    }
+
+    /**
+     * Retrieve params from publisher - DateTimeRange
+     * @memberof APIMApiThrottleAnalyticsWidget
+     * */
+    handlePublisherParameters(receivedMsg) {
+        const queryParam = super.getGlobalState('dtrp');
+        const { sync } = queryParam; const {
+            from, to, granularity, dm, op,
+        } = receivedMsg;
+
+        if (dm && from) {
+            this.setState({
+                dimension: dm,
+                selectedOptions: op,
+                timeFrom: from,
+                timeTo: to,
+                perValue: granularity,
+                inProgress: !sync,
+            }, this.assembleMainQuery);
+        } else if (dm) {
+            this.setState({
+                dimension: dm,
+                selectedOptions: op,
+                inProgress: true,
+            }, this.assembleMainQuery);
+        } else if (from) {
+            this.setState({
+                timeFrom: from,
+                timeTo: to,
+                perValue: granularity,
+                inProgress: !sync,
+            }, this.assembleMainQuery);
+        }
+    }
+
+    /**
+     * Formats the siddhi query - mainquery
+     * @memberof APIMApiThrottleAnalyticsWidget
+     * */
+    assembleMainQuery() {
+        const {
+            providerConfig, timeFrom, timeTo, perValue, dimension, selectedOptions,
+        } = this.state;
+        const { widgetID: widgetName, id } = this.props;
+        if (dimension && timeFrom) {
+            if (selectedOptions && selectedOptions.length > 0) {
+                const filterCondition = '(apiName==\'' + selectedOptions[0].name + '\' AND apiVersion==\''
+                    + selectedOptions[0].version + '\' AND apiCreator==\'' + selectedOptions[0].provider + '\')';
+
+                const dataProviderConfigs = cloneDeep(providerConfig);
+                dataProviderConfigs.configs.config.queryData.queryName = 'query';
+                dataProviderConfigs.configs.config.queryData.queryValues = {
+                    '{{timeFrom}}': timeFrom,
+                    '{{timeTo}}': timeTo,
+                    '{{per}}': perValue,
+                    '{{filterCondition}}': filterCondition,
+                };
+                super.getWidgetChannelManager()
+                    .subscribeWidget(id, widgetName, this.handleDataReceived, dataProviderConfigs);
+            } else {
+                this.setState({ inProgress: false, throttleData: [], tableData: [] });
+            }
+        }
+    }
+
+    /**
+     * Formats data retrieved from assembleMainQuery
+     * @param {object} message - data retrieved
+     * @memberof APIMApiThrottleAnalyticsWidget
+     * */
+    handleDataReceived(message) {
+        const { data } = message;
+        if (data && data.length > 0) {
+            const tableData = data.map((dataUnit) => {
+                return ({
+                    appname: dataUnit[0],
+                    throttles: dataUnit[1],
+                    reason: dataUnit[2].split('_').join(' ').toLowerCase(),
+                    reqtime: Moment(dataUnit[3]).format('YYYY-MMM-DD hh:mm:ss A'),
+                });
+            });
+            const dataGroupByTime = data.reduce((acc, obj) => {
+                const key = obj[3];
+                if (!acc[key]) {
+                    acc[key] = 0;
+                }
+                acc[key] += obj[1];
+                return acc;
+            }, {});
+            const throttleData = Object.keys(dataGroupByTime).map((key) => {
+                return [dataGroupByTime[key], parseInt(key, 10)]; });
+            this.setState({ throttleData, tableData, inProgress: false });
+        } else {
+            this.setState({ inProgress: false, throttleData: [], tableData: [] });
+        }
+    }
+
+    /**
+     * @inheritDoc
+     * @returns {ReactElement} Render the APIM Api Throttle Analytics widget
+     * @memberof APIMApiThrottleAnalyticsWidget
+     */
+    render() {
+        const {
+            localeMessages, faultyProviderConfig, chartConfig, metadata, height, width, inProgress, throttleData,
+            tableData,
+        } = this.state;
+        const {
+            paper, paperWrapper,
+        } = this.styles;
+        const { muiTheme } = this.props;
+        const themeName = muiTheme.name;
+        const faultProps = {
+            themeName,
+            chartConfig,
+            metadata,
+            height,
+            width,
+            throttleData,
+            tableData,
+            inProgress,
+        };
+
+        return (
+            <IntlProvider locale={language} messages={localeMessages}>
+                <MuiThemeProvider theme={themeName === 'dark' ? darkTheme : lightTheme}>
+                    <div>
+                        {
+                            faultyProviderConfig ? (
+                                <div style={paperWrapper}>
+                                    <Paper
+                                        elevation={1}
+                                        style={paper}
+                                    >
+                                        <Typography variant='h5' component='h3'>
+                                            <FormattedMessage
+                                                id='config.error.heading'
+                                                defaultMessage='Configuration Error !'
+                                            />
+                                        </Typography>
+                                        <Typography component='p'>
+                                            <FormattedMessage
+                                                id='config.error.body'
+                                                defaultMessage={'Cannot fetch provider configuration for APIM '
+                                                + 'Api Throttle Analytics widget'}
+                                            />
+                                        </Typography>
+                                    </Paper>
+                                </div>
+                            ) : (
+                                <APIMApiThrottleAnalytics
+                                    {...faultProps}
+                                />
+                            )
+                        }
+                    </div>
+                </MuiThemeProvider>
+            </IntlProvider>
+        );
+    }
+}
+
+global.dashboard.registerWidget('APIMApiThrottleAnalytics', APIMApiThrottleAnalyticsWidget);
