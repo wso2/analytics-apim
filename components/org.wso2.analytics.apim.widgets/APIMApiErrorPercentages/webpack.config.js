c2b5bafff18cf318ebc67ce2350fb05dcfcc3700
TanyaM <tanyamadurapperuma@gmail.com>
2020-05-09 18:48:35 +0530

    Add material ui table libs to Api Error Percentages widget

--- components/org.wso2.analytics.apim.widgets/APIMApiErrorPercentages/webpack.config.js
+++ components/org.wso2.analytics.apim.widgets/APIMApiErrorPercentages/webpack.config.js
@@ -57,6 +57,7 @@ module.exports = {
                 loader: 'babel-loader',
                 query: {
                     presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins: ['@babel/plugin-proposal-class-properties'],
                 },
             },
             {
