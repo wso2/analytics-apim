6b4daf1f43ac36f088f93f7004051f297e0e9981
ketharan <sketharan1996.15@cse.mrt.ac.lk>
2020-05-23 18:46:58 +0530

    Adds pdf and csv export to APIMAppCreatedAnalytics widget.

--- components/org.wso2.analytics.apim.widgets/APIMAppCreatedAnalytics/src/APIMAppCreatedData.jsx
+++ components/org.wso2.analytics.apim.widgets/APIMAppCreatedAnalytics/src/APIMAppCreatedData.jsx
@@ -77,6 +77,14 @@ export default function APIMAppCreatedData(props) {
             marginTop: '3%',
         },
     };
+    const columns = [
+        {
+            id: 'appname', numeric: false, disablePadding: false, label: 'table.heading.appname',
+        },
+        {
+            id: 'createdtime', numeric: false, disablePadding: false, label: 'table.heading.createdtime',
+        },
+    ];
 
     if (tableData.length !== 0 && chartData.length !== 0) {
         return (
@@ -207,6 +215,7 @@ export default function APIMAppCreatedData(props) {
                 <div style={styles.tableWrapper}>
                     <CustomTable
                         data={tableData}
+                        columns={columns}
                     />
                 </div>
             </div>
